7b77f3b31f70dbc14ba10b96e16a166fe4988fcf
fatumeh <maazim476@gmail.com>
2026-02-13 22:14:02 +0300

    Done

--- colorChanger/app.js
+++ colorChanger/app.js
@@ -1,19 +1,27 @@
+/* marka hore waxan sameyey variable bntcolor is aan uso aqrista buttomka */
 const btnColor = document.getElementById("btnColor");
 
+/* 2. Waxaan ku xiray 'addEventListener' si uu u 
+dhageysto marka button-ka la riixo 
+ ka dibna u fuliyo logic-ga gudaha ku jira */
 btnColor.addEventListener("click", () => {
-    const colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-    let hex = "#";
-
-    // We need to loop 6 times to get 6 characters (e.g., #FFAA00)
-    for (let i = 0; i < 6; i++) {
-        // Pick a random index from 0 to 15
-        let randomIndex = Math.floor(Math.random() * colors.length);
-        hex += colors[randomIndex];
-    }
-
-    // Apply it to the body
-    document.body.style.backgroundColor = hex;
-    let hexCode = document.getElementById("hexCode");
-    hexCode.innerText = hex;
-    // Optional: Log it so you can see what's happening!
+  /* 3. Waxaan sameeyay array ka koban letters iyo numbers laga samenaya Hex Colorka */
+  const colors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+  /* 4. Hex colorku wuxuu  ku bilowdaa  # markaa variablekan aya no qabaya */
+  let hex = "#";
+  /* next Waxan isticmaly for loop si uu 6 jeer u soo dhex 
+    warego arrayda maadaama Hex Codeku ka koban yahay 6 digit */
+  for (let i = 0; i < 6; i++) {
+    /* waxan sameyey index random ah: 
+                Math.random() wuxuu so sara nambar u dhexeya 0 ilaa 1
+                Waxaan ku dhuftay dhererka arrayda and then si aan u helno nambar
+                 Math.floor() wuxuu ka saraya jajabka   */
+    let randomIndex = Math.floor(Math.random() * colors.length);
+    hex += colors[randomIndex];
+  }
+  /* finaly waxan bedelnay colorka backgrounka
+   sido kale waxana soo bandhignay code-ka midabkaas */
+  document.body.style.backgroundColor = hex;
+  let hexCode = document.getElementById("hexCode");
+  hexCode.innerText = hex;
 });
